7b3deed29b3422cee5038bad66c113a31a3d6b8e
lizhen <lizhen@gaodun.com>
2019-07-15 11:28:09 +0800

    update

--- src/components/TsDemo.tsx
+++ src/components/TsDemo.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 const TsDemo = () => {
     const el = document.getElementsByClassName('tsDemo');
+    console.log('dd');
     return (
         <div className='tsDemo'>
             this is a tsDemo
